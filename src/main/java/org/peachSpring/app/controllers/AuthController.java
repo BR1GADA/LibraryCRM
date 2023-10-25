697fcaff870e495339ef83df926a7a71923b02b4
онстантин ильчитский <kilatos@yandex.ru>
2023-10-25 12:25:25 +0300

    adding custom auth page

---
+++ src/main/java/org/peachSpring/app/controllers/AuthController.java
@@ -0,0 +1,19 @@
+package org.peachSpring.app.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String showLoginPage(){
+        return "auth/login";
+    }
+}
--- src/main/java/org/peachSpring/app/security/SecurityConfig.java
+++ src/main/java/org/peachSpring/app/security/SecurityConfig.java
@@ -3,7 +3,15 @@ package org.peachSpring.app.security;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 
 @EnableWebSecurity
@@ -13,5 +21,24 @@ public class SecurityConfig  {
     @Autowired
     public SecurityConfig(AuthProviderImpl authProvider) {
         this.authProvider = authProvider;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers("/auth/login","/styles/styleForAuth.css")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin(form->form.loginPage("/auth/login").loginProcessingUrl("/login").defaultSuccessUrl("/")
+
+                 );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
